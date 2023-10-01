5e498a15ca3ef1a5d5d17bc92a33d8720fc06a4c
이성희 <the.paper@live.co.kr>
2023-10-01 21:51:00 +0900

    https://devu.tistory.com/59

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,27 +4,34 @@ const ACCELERATION = 400
 const MAX_SPEED = 100
 const FRICTION = 400
 
-var moveVelocity = Vector2.ZERO
+var input_vector = Vector2.ZERO
 
 var animationPlayer: AnimationPlayer = null
+var animationTree: AnimationTree = null
+var animationState: AnimationNodeStateMachinePlayback = null
  
 func _ready():
 	animationPlayer = $AnimationPlayer
+	animationTree = $AnimationTree
+	animationState = animationTree.get("parameters/playback")
  
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector.normalized()
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
+
+func _process(delta):
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+	else:
+		animationState.travel("Idle")
+	
